16dadbf04b61c097e0c52958469e1b83235c8be0
atilaalcantara <atila.alcantara@gavresorts.com.br>
2025-02-21 14:25:17 -0300

    fix: comparing with goals, not contributions

--- PairProgress.Backend/Services/BuddyService.cs
+++ PairProgress.Backend/Services/BuddyService.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using PairProgress.Backend.Data;
+using PairProgress.Backend.Models;
 using PairProgress.Backend.Models.Enum;
 using PairProgress.Backend.Services.Interfaces;
 
@@ -16,18 +17,36 @@ public class BuddyService :IBuddyService
 
     public async Task<BuddyMoodEnum> GetBuddyMoodForUserAsync(string userCode)
     {
+        var userCodesTuple = (userCode, userCode);
+
+        var userDuoDb = _dbContext.UserDuos.FirstOrDefault(ud => ud.User1Code == userCode || ud.User2Code == userCode);
+
+        if (userDuoDb != null)
+        {
+            userCodesTuple = (userDuoDb.User1Code, userDuoDb.User2Code);
+        }
+        
+        var goals = await _dbContext.Goals
+            .Include(g => g.User)
+            .Where(g => g.User.UserCode == userCodesTuple.Item1 || g.User.UserCode == userCodesTuple.Item2)
+            .ToListAsync();
+        
         var contributions = await _dbContext.Contributions
             .Where(c => c.User.UserCode == userCode)
             .ToListAsync();
 
-        var contributionsOnThisMonth = contributions
-            .Count(c => c.Date.Month == DateTime.Now.Month);
-        
+        if (contributions == null || contributions.Count == 0)
+        {
+            return BuddyMoodEnum.Angry;
+        }
+
+        var contributionsOnThisMonth = contributions.Count(c => c.Date.Month == DateTime.Now.Month && c.Date.Year == DateTime.Now.Year);
+
         if (contributionsOnThisMonth == 0)
         {
             return BuddyMoodEnum.Angry;
         }
-        else if(contributionsOnThisMonth >= (contributions.Count/2))
+        else if (contributionsOnThisMonth >= (int)Math.Ceiling(goals.Count / 2.0))
         {
             return BuddyMoodEnum.Happy;
         }
