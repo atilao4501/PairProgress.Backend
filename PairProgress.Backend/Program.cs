7b73fb691786dc2e0ee01ee929318ac3483952c9
atilaalcantara <atila.alcantara@gavresorts.com.br>
2025-02-28 14:48:21 -0300

    feat: swagger in release

--- PairProgress.Backend/Program.cs
+++ PairProgress.Backend/Program.cs
@@ -89,14 +89,18 @@ builder.Services.AddIdentityApiEndpoints<User>()
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.Migrate();
+}
 
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseHttpsRedirection();
 app.UseCors("CORSPolicy");
